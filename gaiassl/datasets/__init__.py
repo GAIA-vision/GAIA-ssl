0e877a81576e48af49b84e817a14532ad3a16e8f
Qing Chang <2292886710@qq.com>
2022-04-03 10:35:44 +0800

    Update __init__.py

--- gaiassl/datasets/__init__.py
+++ gaiassl/datasets/__init__.py
@@ -1,6 +1,4 @@
 from .adjust_dataset import *
 from .different_pipeline import DifferentPipeline
 from .augmentations import *
-from .detco_pipeline import DetCoPipeline
-from .detcon_pipeline import DetConPipeline
 from .dense_relation_dataset import DenseRelationDataset
